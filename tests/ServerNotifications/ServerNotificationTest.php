bdd9571ae50dd86178ff760927f3acea7de88aa8
imdhemy_cb <imdhemy@gmail.com>
2021-04-03 16:59:04 +0200

    feature(notifications): support revoke notification

--- tests/ServerNotifications/ServerNotificationTest.php
+++ tests/ServerNotifications/ServerNotificationTest.php
@@ -18,4 +18,18 @@ class ServerNotificationTest extends TestCase
         $serverNotification = ServerNotification::fromArray($serverNotificationBody);
         $this->assertInstanceOf(ServerNotification::class, $serverNotification);
     }
+
+    /**
+     * @test
+     */
+    public function test_it_creates_revoke_notification()
+    {
+        $path = realpath(__DIR__ . '/../../server-notification.json');
+        $serverNotificationBody = json_decode(file_get_contents($path), true);
+        $serverNotificationBody['notification_type'] = ServerNotification::REVOKE;
+
+        $serverNotification = ServerNotification::fromArray($serverNotificationBody);
+        $this->assertInstanceOf(ServerNotification::class, $serverNotification);
+        $this->assertEquals(ServerNotification::REVOKE, $serverNotification->getNotificationType());
+    }
 }
