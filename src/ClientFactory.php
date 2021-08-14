af5c5b5dfe783507614afa71d08350a1b9091943
imdhemy <imdhemy@gmail.com>
2021-08-14 12:03:27 +0200

    feat(client): allow client mocks

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -3,6 +3,10 @@
 namespace Imdhemy\AppStore;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use Psr\Http\Client\RequestExceptionInterface;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Class ClientFactory
@@ -19,8 +23,8 @@ class ClientFactory
     public static function create(bool $sandbox = false): Client
     {
         return new Client([
-            'base_uri' => $sandbox ? self::BASE_URI_SANDBOX : self::BASE_URI,
-        ]);
+                            'base_uri' => $sandbox ? self::BASE_URI_SANDBOX : self::BASE_URI,
+                          ]);
     }
 
     /**
@@ -29,5 +33,47 @@ class ClientFactory
     public static function createSandbox(): Client
     {
         return self::create(true);
+    }
+
+    /**
+     * Creates a client that returns the specified response
+     *
+     * @param ResponseInterface $responseMock
+     * @return Client
+     */
+    public static function mock(ResponseInterface $responseMock): Client
+    {
+        $mockHandler = new MockHandler([$responseMock]);
+        $handlerStack = HandlerStack::create($mockHandler);
+
+        return new Client(['handler' => $handlerStack]);
+    }
+
+    /**
+     * Creates a client that returns the specified array of responses in queue order
+     *
+     * @param array|ResponseInterface[]|RequestExceptionInterface[] $responseQueue
+     * @return Client
+     */
+    public static function mockQueue(array $responseQueue): Client
+    {
+        $mockHandler = new MockHandler($responseQueue);
+        $handlerStack = HandlerStack::create($mockHandler);
+
+        return new Client(['handler' => $handlerStack]);
+    }
+
+    /**
+     * Creates a client that returns the specified request exception
+     *
+     * @param RequestExceptionInterface $error
+     * @return Client
+     */
+    public static function mockError(RequestExceptionInterface $error): Client
+    {
+        $mockHandler = new MockHandler([$error]);
+        $handlerStack = HandlerStack::create($mockHandler);
+
+        return new Client(['handler' => $handlerStack]);
     }
 }
--- tests/ClientFactoryTest.php
+++ tests/ClientFactoryTest.php
@@ -2,6 +2,11 @@
 
 namespace Imdhemy\AppStore\Tests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 use Imdhemy\AppStore\ClientFactory;
 
 class ClientFactoryTest extends TestCase
@@ -11,8 +16,66 @@ class ClientFactoryTest extends TestCase
      */
     public function test_create()
     {
-        $client = ClientFactory::createSandbox();
-        $baseUri = (string)$client->getConfig()['base_uri'];
-        $this->assertEquals(ClientFactory::BASE_URI_SANDBOX, $baseUri);
+        $client = ClientFactory::create();
+        $this->assertInstanceOf(Client::class, $client);
+    }
+
+    /**
+     * @test
+     * @throws GuzzleException
+     */
+    public function test_client_response_can_be_mocked()
+    {
+        $statusCode = 200;
+        $body = 'This is a mock!';
+        $mock = new Response($statusCode, [], $body);
+
+        $client = ClientFactory::mock($mock);
+        $response = $client->request('GET', '/');
+
+        $this->assertEquals($statusCode, $response->getStatusCode());
+        $this->assertEquals($body, (string)$response->getBody());
+    }
+
+    /**
+     * @test
+     * @throws GuzzleException
+     */
+    public function test_a_queue_of_responses_can_be_mocked()
+    {
+        $mocks = [
+          new Response(200, [], 'first'),
+          new Response(201, [], 'second'),
+        ];
+        $client = ClientFactory::mockQueue($mocks);
+
+        $firstResponse = $client->request('GET', '/');
+        $this->assertEquals(200, $firstResponse->getStatusCode());
+        $this->assertEquals('first', (string)$firstResponse->getBody());
+
+        $secondResponse = $client->request('POST', '/');
+        $this->assertEquals(201, $secondResponse->getStatusCode());
+        $this->assertEquals('second', (string)$secondResponse->getBody());
+    }
+
+    /**
+     * @test
+     * @throws GuzzleException
+     */
+    public function test_it_can_mock_an_error_response()
+    {
+        $message = 'Something went wrong';
+
+        $this->expectException(RequestException::class);
+        $this->expectExceptionMessage($message);
+
+        $error = new RequestException(
+          $message,
+          new Request('GET', '/admin'),
+          new Response(403, [], 'Forbidden')
+        );
+        $client = ClientFactory::mockError($error);
+
+        $client->request('GET', '/admin');
     }
 }
