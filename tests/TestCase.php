6529eeef0b4874f34b0558a370ff14f0978e5496
imdhemy <imdhemy@gmail.com>
2021-08-14 18:25:18 +0200

    chore(tests): add fixtures

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,19 @@ namespace Imdhemy\AppStore\Tests;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @return false|string
+     */
+    protected function getSubscriptionReceipt()
+    {
+        return file_get_contents(__DIR__ . '/../fixtures/subscription_receipt.json');
+    }
+
+    /**
+     * @return false|string
+     */
+    protected function getVerifyReceiptResponse()
+    {
+        return file_get_contents(__DIR__ . '/../fixtures/verify_receipt_response.json');
+    }
 }
