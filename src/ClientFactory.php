6a941544b1a0611ab25393640545544080c720f1
imdhemy <imdhemy@gmail.com>
2021-08-15 11:19:16 +0200

    chore(test): cover client factory with tests

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -5,6 +5,7 @@ namespace Imdhemy\AppStore;
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
 use Psr\Http\Client\RequestExceptionInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -13,8 +14,8 @@ use Psr\Http\Message\ResponseInterface;
  */
 class ClientFactory
 {
-    const BASE_URI = 'https://buy.itunes.apple.com';
-    const BASE_URI_SANDBOX = 'https://sandbox.itunes.apple.com';
+    public const BASE_URI = 'https://buy.itunes.apple.com';
+    public const BASE_URI_SANDBOX = 'https://sandbox.itunes.apple.com';
 
     /**
      * @param bool $sandbox
@@ -22,11 +23,7 @@ class ClientFactory
      */
     public static function create(bool $sandbox = false): Client
     {
-        return new Client(
-            [
-            'base_uri' => $sandbox ? self::BASE_URI_SANDBOX : self::BASE_URI,
-          ]
-        );
+        return new Client(['base_uri' => $sandbox ? self::BASE_URI_SANDBOX : self::BASE_URI]);
     }
 
     /**
@@ -41,12 +38,14 @@ class ClientFactory
      * Creates a client that returns the specified response
      *
      * @param ResponseInterface $responseMock
+     * @param array $transactions
      * @return Client
      */
-    public static function mock(ResponseInterface $responseMock): Client
+    public static function mock(ResponseInterface $responseMock, array &$transactions = []): Client
     {
         $mockHandler = new MockHandler([$responseMock]);
         $handlerStack = HandlerStack::create($mockHandler);
+        $handlerStack->push(Middleware::history($transactions));
 
         return new Client(['handler' => $handlerStack]);
     }
@@ -55,12 +54,14 @@ class ClientFactory
      * Creates a client that returns the specified array of responses in queue order
      *
      * @param array|ResponseInterface[]|RequestExceptionInterface[] $responseQueue
+     * @param array $transactions
      * @return Client
      */
-    public static function mockQueue(array $responseQueue): Client
+    public static function mockQueue(array $responseQueue, array &$transactions = []): Client
     {
         $mockHandler = new MockHandler($responseQueue);
         $handlerStack = HandlerStack::create($mockHandler);
+        $handlerStack->push(Middleware::history($transactions));
 
         return new Client(['handler' => $handlerStack]);
     }
@@ -69,12 +70,14 @@ class ClientFactory
      * Creates a client that returns the specified request exception
      *
      * @param RequestExceptionInterface $error
+     * @param array $transactions
      * @return Client
      */
-    public static function mockError(RequestExceptionInterface $error): Client
+    public static function mockError(RequestExceptionInterface $error, array &$transactions = []): Client
     {
         $mockHandler = new MockHandler([$error]);
         $handlerStack = HandlerStack::create($mockHandler);
+        $handlerStack->push(Middleware::history($transactions));
 
         return new Client(['handler' => $handlerStack]);
     }
--- tests/ClientFactoryTest.php
+++ tests/ClientFactoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Imdhemy\AppStore\Tests;
 
+use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\RequestException;
@@ -17,6 +18,9 @@ class ClientFactoryTest extends TestCase
     public function test_create()
     {
         $client = ClientFactory::create();
+        $this->assertInstanceOf(Client::class, $client);
+
+        $client = ClientFactory::createSandbox();
         $this->assertInstanceOf(Client::class, $client);
     }
 
@@ -44,8 +48,8 @@ class ClientFactoryTest extends TestCase
     public function test_a_queue_of_responses_can_be_mocked()
     {
         $mocks = [
-          new Response(200, [], 'first'),
-          new Response(201, [], 'second'),
+            new Response(200, [], 'first'),
+            new Response(201, [], 'second'),
         ];
         $client = ClientFactory::mockQueue($mocks);
 
@@ -77,5 +81,69 @@ class ClientFactoryTest extends TestCase
         $client = ClientFactory::mockError($error);
 
         $client->request('GET', '/admin');
+    }
+
+    /**
+     * @test
+     * @throws GuzzleException
+     */
+    public function test_mock_can_track_transactions()
+    {
+        $transactions = [];
+        $response = new Response();
+
+        $client = ClientFactory::mock($response, $transactions);
+        $client->request('GET', '/mock');
+
+        $transactionResponse = $transactions[0]['response'];
+        $this->assertSame($response, $transactionResponse);
+    }
+
+    /**
+     * @test
+     * @throws Exception
+     * @throws GuzzleException
+     */
+    public function test_mock_queue_can_track_transactions()
+    {
+        $size = random_int(1, 10);
+        $queue = [];
+
+        for ($i = 0; $i < $size; $i++) {
+            $queue[] = new Response(200, [], sprintf('Response #%d', $i));
+        }
+
+        $transactions = [];
+        $client = ClientFactory::mockQueue($queue, $transactions);
+
+        for ($i = 0; $i < $size; $i++) {
+            $client->request('GET', sprintf('/foo-%s', $i));
+        }
+
+        for ($i = 0; $i < $size; $i++) {
+            $expectedResponse = $queue[$i];
+            $transactionResponse = $transactions[$i]['response'];
+            $this->assertSame($expectedResponse, $transactionResponse);
+        }
+    }
+
+    /**
+     * @test
+     * @throws GuzzleException
+     */
+    public function test_mock_error_can_track_transactions()
+    {
+        $transactions = [];
+        $request = new Request('GET', '/admin');
+        $response = new Response(403, [], 'Forbidden');
+
+        $error = new RequestException('Something went wrong', $request, $response);
+        $client = ClientFactory::mockError($error, $transactions);
+        try {
+            $client->get('/admin');
+        } catch (RequestException $exception) {
+            $error = $transactions[0]['error'];
+            $this->assertSame($exception, $error);
+        }
     }
 }
