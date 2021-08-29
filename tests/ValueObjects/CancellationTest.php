cd445195f0997c7486e54f46e3c62955d82df836
imdhemy_cb <imdhemy@gmail.com>
2021-08-29 12:43:18 +0200

    fix(cancellation): fix reason values and cover with test

---
+++ tests/ValueObjects/CancellationTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Imdhemy\AppStore\Tests\ValueObjects;
+
+use Exception;
+use Imdhemy\AppStore\ValueObjects\Cancellation;
+use Imdhemy\AppStore\ValueObjects\Time;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * It's obvious that there is no need to test getter method
+ * but this test was added to maintain the convention of cover every class
+ * with a unit test.
+ */
+class CancellationTest extends TestCase
+{
+    /**
+     * @test
+     * @throws Exception
+     */
+    public function test_basic_usage()
+    {
+        $time = new Time(time() * 1000);
+        $reason = [Cancellation::REASON_OTHER, Cancellation::REASON_APP_ISSUE][random_int(0, 1)];
+
+        $cancellation = new Cancellation($time, $reason);
+
+        $this->assertSame($time, $cancellation->getTime());
+        $this->assertSame($reason, $cancellation->getReason());
+
+        if ($reason === Cancellation::REASON_APP_ISSUE) {
+            $this->assertTrue($cancellation->isDueAppIssue());
+            $this->assertFalse($cancellation->isDueAnotherReason());
+        }
+
+        if ($reason === Cancellation::REASON_OTHER) {
+            $this->assertTrue($cancellation->isDueAnotherReason());
+            $this->assertFalse($cancellation->isDueAppIssue());
+        }
+    }
+}
