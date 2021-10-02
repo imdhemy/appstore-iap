cd8e992ce420ae620dd3b07a4d6d435c8a454906
Mohammad ALTAWEEL <hello@mohammad.tech>
2021-10-02 09:22:09 +0300

    Get expiration intent value

--- src/ValueObjects/ExpirationIntent.php
+++ src/ValueObjects/ExpirationIntent.php
@@ -5,14 +5,25 @@ namespace Imdhemy\AppStore\ValueObjects;
 
 final class ExpirationIntent
 {
+    /**
+     * @var int
+     */
     private $value;
 
     /**
      * ExpirationIntent constructor.
-     * @param $value
+     * @param int $value
      */
-    public function __construct($value)
+    public function __construct(int $value)
     {
         $this->value = $value;
+    }
+
+    /**
+     * @return int
+     */
+    public function getValue(): int
+    {
+        return $this->value;
     }
 }
