f65e7036c2d48c49f235603bde0a56c7d78b96ce
Mohamad Eldhemy <imdhemy@gmail.com>
2022-09-18 21:43:41 +0200

    feat(notif-v2): create from signed payload

--- src/ServerNotifications/V2DecodedPayload.php
+++ src/ServerNotifications/V2DecodedPayload.php
@@ -2,13 +2,16 @@
 
 namespace Imdhemy\AppStore\ServerNotifications;
 
+use Imdhemy\AppStore\Contracts\Arrayable;
+use Imdhemy\AppStore\Jws\JsonWebSignature;
+
 /**
  * Class V2DecodedPayload
  * A decoded payload containing the version 2 notification data.
  *
  * @see https://developer.apple.com/documentation/appstoreservernotifications/responsebodyv2decodedpayload
  */
-final class V2DecodedPayload
+final class V2DecodedPayload implements Arrayable
 {
     /**
      * @var string
@@ -41,10 +44,37 @@ final class V2DecodedPayload
     private int $signedDate;
 
     /**
+     * @var array
+     */
+    private array $rawData;
+
+    /**
      * Prevent direct instantiation from outside
      */
-    private function __construct()
+    private function __construct(array $rawData)
     {
-        //
+        $this->rawData = $rawData;
+    }
+
+    /**
+     * Create a new instance from a Json Web Signature
+     *
+     * @param JsonWebSignature $jws
+     *
+     * @return static
+     */
+    public static function fromJws(JsonWebSignature $jws): self
+    {
+        return new self($jws->getClaims());
+    }
+
+    /**
+     * Convert the object to its array representation.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return $this->rawData;
     }
 }
---
+++ tests/Unit/ServerNotifications/V2DecodedPayloadTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Imdhemy\AppStore\Tests\Unit\ServerNotifications;
+
+use Imdhemy\AppStore\Jws\Parser;
+use Imdhemy\AppStore\ServerNotifications\V2DecodedPayload;
+use Imdhemy\AppStore\Tests\TestCase;
+
+class V2DecodedPayloadTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function v2_decoded_payload(): void
+    {
+        $claims = [
+            'notificationType' => 'TEST',
+            'notificationUUID' => $this->faker->uuid(),
+            'data' => [],
+            'version' => '2.0',
+            'signedDate' => $this->faker->unixTime() * 1000,
+        ];
+
+        $signedPayload = $this->faker->jwsOf($claims);
+        $jws = Parser::toJws($signedPayload);
+
+        $sut = V2DecodedPayload::fromJws($jws);
+
+        foreach ($claims as $key => $value) {
+            $this->assertEquals($value, $sut->toArray()[$key]);
+        }
+    }
+}
